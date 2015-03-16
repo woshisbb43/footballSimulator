6f3368fa811479e622a7f98533631d1f0454ff5d
mixacom <wmmix@mail.ru>
2015-03-16 23:17:51 +0100

    Revert "Ball data in one file and equaled speed of animation"
    This reverts commit 097b44e61a6d2ef2648921c768f83670b08daaac.
    
    Conflicts:
    	all.txt
    	text.html

--- simpballd3.js
+++ simpballd3.js
@@ -1,67 +1,40 @@
-// making a football pitch with general dimensions from http://en.wikipedia.org/wiki/Association_football_pitch#Other_markings
+/**
+ * Created by Mikhail on 28.02.2015.
+ */
 
-var pi = Math.PI;
-
-var width = 125 * 9;
-var height = 74 * 6.8 ;
-
-
-
-var piter = d3.select('body').append('svg').attr('class', 'pitch').attr('height',height+20) // draw a pitch
+var piter = d3.select('body').append('svg').attr('class', 'pitch').attr('height',height+20)
     .attr('width', width+20)
-    .style('background', 'green');
+    .style('background','green');
 
-
-//centerline of the pitch
-piter.append('line')
-    .attr('x1',width/2+10)
-    .attr('x2',width/2+10)
-    .attr('y1',10)
-    .attr('y2',height+10)
-    .attr('stroke','white');
-
-
-
-
-d3.csv("all.csv", function(error, ballP) {                     // load data for a ball
+d3.csv("part.csv", function(error, ballP) {                     // load data foa a ball
     // for (var i = 0; i < ballP.length; i++) {
     //   console.log(ballP[i].x_pos);
     // };
 
 //path
     var lineFunction = d3.svg.line()
-        .x(function(d) { return (d.x_pos - 900) / 3; })  // approximate coordinates
-        .y(function(d) { return (d.y_pos - 200) / 3; })
+        .x(function (d) {
+            return d.x_pos * 10;
+        })  // approximate coordinates
+        .y(function (d) {
+            return d.y_pos * 10;
+        })
         .interpolate("linear");
+
+    var svgContainer = d3.select("body").append("svg")
+        .attr("width", 600)
+        .attr("height", 600);
 
     var path = piter.append("path")
         .attr("d", lineFunction(ballP))
         .attr("stroke", "blue")
         .attr("stroke-width", 2)
-        .attr("fill", "yellow");
+        .attr("fill", "none");
 
     var circle = piter.append("circle")                             // draw a circle
         .attr("r", 20);
-    // .attr("transform" , "translate(" +  +")");
-
-
-    circle.transition()
-            .duration(20000)
-            .attrTween("transform", translateAlong(path.node()));
-
-// Returns an attrTween for translating along the specified path element.
-    function translateAlong(path) {
-        var l = path.getTotalLength();
-
-        console.log(l);
-
-        return function(d, i, a) {
-            return function(t) {
-                var p = path.getPointAtLength(t * l);
-                return "translate(" + p.x + "," + p.y + ")";
-            };
-        };
-    }
 
 });
+
+// .attr("transform" , "translate(" +  +")");
 
